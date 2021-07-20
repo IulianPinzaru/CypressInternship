6e9adf26cb600ceab167881be41cc4cb7d4c5164
IulianPinzaru <62741558+IulianPinzaru@users.noreply.github.com>
2021-07-20 14:35:48 +0300

    Task 1 has been Modified

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,3 +1,4 @@
+
 describe('Register and Login functionality', () => {
   beforeEach(() => {
     
@@ -44,44 +45,64 @@ describe('Register and Login functionality', () => {
       cy.get('#loginFrm_loginname').should('have.class', 'form-control').type('IulianPinzaru')
       cy.get('#loginFrm_password').should('have.class', 'form-control').type('12345678')
       cy.get('#loginFrm > fieldset > button').contains('Login').click()
+
+      if(cy.get('#customer_menu_top > li > a > div').contains('Pinzaru'))
+      {
+        cy.log('Login Succesful')
+      }
+
     })
 
-    it('Validation for Register with invalid data. ( Ascii characters, invalid email, number )',() =>{
+    
 
-      cy.get("#customer_menu_top")
-        .click()
-      
-        cy.get("#accountFrm > fieldset > button")
-        .contains("Continue").click()
+it('Validation for Register with invalid data. ( Ascii characters, invalid email, number )',() =>{
+  
+  cy.get("#customer_menu_top")
+    .click()
+  
+    cy.get("#accountFrm > fieldset > button")
+    .contains("Continue").click()
+  //Tryin' to register with invalid data.
+  cy.get('#AccountFrm_lastname').should('have.class', 'form-control').type('Iulian┘') // Ascii characters
+  cy.get("#AccountFrm_firstname").should('have.class', 'form-control').type('∞²╚±B╞á') // Ascii characters
+  cy.get('#AccountFrm_email').should('have.class', 'form-control').type('iulipinzaru') // invalid email
+  cy.get('#AccountFrm_telephone').should('have.class', 'form-control').type('076966276') // invalid number
+  cy.get('#AccountFrm_fax').should('have.class', 'form-control').type('abcderf') // invalid fax ( only numbers )
+  cy.get('#AccountFrm_company').should('have.class', 'form-control').type('SCSA3∞3') // Ascii characters
+  cy.get('#AccountFrm_address_1').should('have.class', 'form-control').type('STR∞ Tipografiei 1') //Ascii characters
+  cy.get('#AccountFrm_address_2').should('have.class', 'form-control').type('∞') // Ascii characters ( field not required ) 
+  cy.get('#AccountFrm_city').should('have.class', 'form-control').type('Radauti■') // Ascii characters
 
-      //Tryin' to register with invalid data.
-      cy.get('#AccountFrm_lastname').should('have.class', 'form-control').type('Iulian┘') // Ascii characters
-      cy.get("#AccountFrm_firstname").should('have.class', 'form-control').type('∞²╚±B╞á') // Ascii characters
-      cy.get('#AccountFrm_email').should('have.class', 'form-control').type('iulipinzaru') // invalid email
-      cy.get('#AccountFrm_telephone').should('have.class', 'form-control').type('076966276') // invalid number
-      cy.get('#AccountFrm_fax').should('have.class', 'form-control').type('abcderf') // invalid fax ( only numbers )
-      cy.get('#AccountFrm_company').should('have.class', 'form-control').type('SCSA3∞3') // Ascii characters
-      cy.get('#AccountFrm_address_1').should('have.class', 'form-control').type('STR∞ Tipografiei 1') //Ascii characters
-      cy.get('#AccountFrm_address_2').should('have.class', 'form-control').type('∞') // Ascii characters ( field not required ) 
-      cy.get('#AccountFrm_city').should('have.class', 'form-control').type('Radauti■') // Ascii characters
+  cy.get('#AccountFrm_country_id').should('have.class', 'form-control')
+    .select('Romania').should('have.value','175')
+  cy.get('#AccountFrm_zone_id').should('have.class', 'form-control')
+    .select('Suceava').should('have.value', '2714')
 
-      cy.get('#AccountFrm_country_id').should('have.class', 'form-control')
-        .select('Romania').should('have.value','175')
-      cy.get('#AccountFrm_zone_id').should('have.class', 'form-control')
-        .select('Suceava').should('have.value', '2714')
-
-      cy.get('#AccountFrm_postcode').should('have.class', 'form-control').type('000033') //invalid postal code for Country
-      cy.get('#AccountFrm_loginname').should('have.class', 'form-control').type('Iuli∞G≥0') // Ascii characters
-      cy.get('#AccountFrm_password').should('have.class', 'form-control').type('12345678:∞') // Ascii characters
-      cy.get('#AccountFrm_confirm').should('have.class', 'form-control').type('12345678:∞') // Ascii characters
-      cy.get('#AccountFrm_newsletter1').should('have.value', '1').click()
-      cy.get('#AccountFrm_agree').should('have.value', '1').click()
-      cy.get('#AccountFrm > div.form-group > div > div > button').contains('Continue').click()
+  cy.get('#AccountFrm_postcode').should('have.class', 'form-control').type('000033') //invalid postal code for Country
+  cy.get('#AccountFrm_loginname').should('have.class', 'form-control').type('Iuli∞G≥0') // Ascii characters
+  cy.get('#AccountFrm_password').should('have.class', 'form-control').type('12345678:∞') // Ascii characters
+  cy.get('#AccountFrm_confirm').should('have.class', 'form-control').type('12345678:∞') // Ascii characters
+  cy.get('#AccountFrm_newsletter1').should('have.value', '1').click()
+  cy.get('#AccountFrm_agree').should('have.value', '1').click()
+  cy.get('#AccountFrm > div.form-group > div > div > button').contains('Continue').click()
 
 
-      // Result : Login name must be alphanumeric only and between 5 and 64 characters!
-      // Email Address does not appear to be valid!
-    })
+  if(cy.get('.alert'))
+  {
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+      .contains('Login name must be alphanumeric only and between 5 and 64 characters!')
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+      .contains('Email Address does not appear to be valid!')
+  }
+  else
+  {
+      cy.log('It should display an alert')
+  }
+  
+  
+ 
+  
+})
 
     it('Validation for Register with all fields empty.', () => {
 
@@ -95,19 +116,25 @@ describe('Register and Login functionality', () => {
         cy.get('#AccountFrm_agree').should('have.value', '1').click()
         cy.get('#AccountFrm > div.form-group > div > div > button').contains('Continue').click()
 
-        /*
-        RESULT:
-        Login name must be alphanumeric only and between 5 and 64 characters!
-        First Name must be between 1 and 32 characters!
-        Last Name must be between 1 and 32 characters!
-        Email Address does not appear to be valid!
-        Address 1 must be between 3 and 128 characters!
-        City must be between 3 and 128 characters!
-        Zip/postal code must be between 3 and 10 characters!
-        Please select a region / state!
-        Password must be between 4 and 20 characters!
 
-        */
+        if(cy.get('.alert'))
+        {
+        cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+          .contains('Login name must be alphanumeric only and between 5 and 64 characters!')
+          .contains('First Name must be between 1 and 32 characters!')
+          .contains('Last Name must be between 1 and 32 characters!')
+          .contains('Email Address does not appear to be valid!')
+          .contains('Address 1 must be between 3 and 128 characters!')
+          .contains('City must be between 3 and 128 characters!')
+          .contains('Zip/postal code must be between 3 and 10 characters!')
+          .contains('Please select a region / state!')
+          .contains('Password must be between 4 and 20 characters!')
+        }
+        else
+        {
+            cy.log('It should display an alert')
+        }
+      
   })
 
       it('Validation for Register with Boundary values. #1',() =>{
@@ -145,18 +172,22 @@ describe('Register and Login functionality', () => {
         cy.get('#AccountFrm_newsletter1').should('have.value', '1').click()
         cy.get('#AccountFrm_agree').should('have.value', '1').click()
         cy.get('#AccountFrm > div.form-group > div > div > button').contains('Continue').click()
+        
+        if(cy.get('.alert'))
+        {
+        cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+          .contains('First Name must be between 1 and 32 characters!')
+          .contains('Last Name must be between 1 and 32 characters!')
+          .contains('Address 1 must be between 3 and 128 characters!')
+          .contains('City must be between 3 and 128 characters!')
+          .contains('Zip/postal code must be between 3 and 10 characters!')
+          .contains('Password must be between 4 and 20 characters!')
+        }
+        else
+        {
+            cy.log('It should display an alert')
+        }
 
-        /*
-        RESULT:
-        Login name must be alphanumeric only and between 5 and 64 characters!
-        First Name must be between 1 and 32 characters!
-        Last Name must be between 1 and 32 characters!
-        Address 1 must be between 3 and 128 characters!
-        City must be between 3 and 128 characters!
-        Zip/postal code must be between 3 and 10 characters!
-        Password must be between 4 and 20 characters!
-
-        */
 
       })
 
@@ -194,17 +225,20 @@ describe('Register and Login functionality', () => {
         cy.get('#AccountFrm_agree').should('have.value', '1').click()
         cy.get('#AccountFrm > div.form-group > div > div > button').contains('Continue').click()
 
-        /*
-        RESULT:
-        Login name must be alphanumeric only and between 5 and 64 characters!
-        First Name must be between 1 and 32 characters!
-        Last Name must be between 1 and 32 characters!
-        Address 1 must be between 3 and 128 characters!
-        City must be between 3 and 128 characters!
-        Zip/postal code must be between 3 and 10 characters!
-        Password must be between 4 and 20 characters!
-
-        */
+        if(cy.get('.alert'))
+        {
+        cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+          .contains('First Name must be between 1 and 32 characters!')
+          .contains('Last Name must be between 1 and 32 characters!')
+          .contains('Address 1 must be between 3 and 128 characters!')
+          .contains('City must be between 3 and 128 characters!')
+          .contains('Password must be between 4 and 20 characters!')
+        }
+        else
+        {
+            cy.log('It should display an alert')
+        }
+        // it pass for postal code > 11
 
       })
       
@@ -217,11 +251,16 @@ describe('Register and Login functionality', () => {
         cy.get('#loginFrm_loginname').should('have.class', 'form-control').type('IulianPinzaru∞')
         cy.get('#loginFrm_password').should('have.class', 'form-control').type('12345678')
         cy.get('#loginFrm > fieldset > button').contains('Login').click()
-
-        //RESULT: Error: Incorrect login or password provided.
-
+        
+        if(cy.get('.alert'))
+        {
+            cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+            .contains('Error: Incorrect login or password provided.')
+        }
+    
+    
       })
-
+    
       it('Validation for Login functionality with Email istead of Login name.', () => {
         cy.get("#customer_menu_top")
         .click()
@@ -229,8 +268,13 @@ describe('Register and Login functionality', () => {
         cy.get('#loginFrm_loginname').should('have.class', 'form-control').type('iulipinzaru@gmail.com')
         cy.get('#loginFrm_password').should('have.class', 'form-control').type('12345678')
         cy.get('#loginFrm > fieldset > button').contains('Login').click()
-
-        //RESULT: Error: Incorrect login or password provided.
+    
+        if(cy.get('.alert'))
+        {
+            cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+            .contains('Error: Incorrect login or password provided.')
+        }
+    
       })
       
       it('Validation for Login functionality with wrong password.', () => {
@@ -240,20 +284,30 @@ describe('Register and Login functionality', () => {
         cy.get('#loginFrm_loginname').should('have.class', 'form-control').type('IulianPinzaru')
         cy.get('#loginFrm_password').should('have.class', 'form-control').type('123456789')
         cy.get('#loginFrm > fieldset > button').contains('Login').click()
-
-        //RESULT: Error: Incorrect login or password provided.
+    
+        if(cy.get('.alert'))
+        {
+            cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+            .contains('Error: Incorrect login or password provided.')
+        }
+    
       })
-
-
+    
+    
       it('Validation for Login Functionality with all data empty', () => {
         cy.get("#customer_menu_top")
         .click()
         // Tryin' to connect all data empty.
         cy.get('#loginFrm > fieldset > button').contains('Login').click()
-
-        //RESULT: Error: Incorrect login or password provided.
+    
+        if(cy.get('.alert'))
+        {
+            cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+            .contains('Error: Incorrect login or password provided.')
+        }
+    
       })
-
+    
       it('Validation for Login Functionality with invalid name and password.', () => {
         cy.get("#customer_menu_top")
         .click()
@@ -261,10 +315,15 @@ describe('Register and Login functionality', () => {
         cy.get('#loginFrm_loginname').should('have.class', 'form-control').type('IulianPinzaru2')
         cy.get('#loginFrm_password').should('have.class', 'form-control').type('123456781')
         cy.get('#loginFrm > fieldset > button').contains('Login').click()
-
-        //RESULT: Error: Incorrect login or password provided.
+    
+        if(cy.get('.alert'))
+        {
+            cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger')
+            .contains('Error: Incorrect login or password provided.')
+        }
       })
 
 
 
 })
+
