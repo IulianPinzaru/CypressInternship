e680af4d77544dd4ba3589f8b84c40e3039b5515
IulianPinzaru <62741558+IulianPinzaru@users.noreply.github.com>
2021-07-22 17:12:24 +0300

    Api Req

--- cypress/integration/apireq.spec.js
+++ cypress/integration/apireq.spec.js
@@ -1,3 +1,4 @@
+var res = []
 describe('API Request', ()=>{
     beforeEach(() => {
       
@@ -6,22 +7,14 @@ describe('API Request', ()=>{
 
     it('Api Request', ()=>{
         cy.request('https://petstore.swagger.io/v2/store/inventory').then((res) =>{
-            console.log(res.body)
 
-            let availableproducts = res.body.available
-            let sold = res.body.sold
-            let unavailable = res.body.unavailable
-            let free = res.body.free
-            let pending = res.body.pending
-            let string = res.body.string
+            Object.entries(res.body).forEach(element =>{
+                cy.log(element[0] + ' ' + element[1])
+            })
 
-            cy.log('Available Products: ', availableproducts)
-            cy.log('Sold items: ', sold)
-            cy.log('Unavailable items: ', unavailable)
-            cy.log('Free: ', free)
-            cy.log('Pending: ', pending)
-            cy.log('String ', string)
+            });
+
+            
 
         })
     })
-})
