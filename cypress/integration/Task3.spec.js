3b4bc692da79bedb1e70f986398315f477c03e37
IulianPinzaru <62741558+IulianPinzaru@users.noreply.github.com>
2021-07-22 12:13:52 +0300

    Task 3

--- cypress/integration/Task3.spec.js
+++ cypress/integration/Task3.spec.js
@@ -1,12 +1,12 @@
 var search = 'CURLS TO STRAIGHT SHAMPOO';
-var orderid, email, i, str = [];
+var orderid = [], email, i, str = [];
 describe('Register and Login functionality', () => {
     beforeEach(() => {
       
       cy.visit('https://automationteststore.com/')
     })
 
-    it.only('Search an Item', ()=>{
+    it('Search an Item', ()=>{
         //Search
         cy.get('#filter_keyword').type(search).type('{enter}')
         //Add item
@@ -28,27 +28,65 @@ describe('Register and Login functionality', () => {
 
         cy.get('.contentpanel > :nth-child(1) > .table > tbody > tr > :nth-child(1)').invoke('text')
         .then((text) => {
-            cy.log(text)
-            str = text.split(' ')
-            cy.log(str)
-            
+            orderid = text.substring(text.indexOf('#')+ '#'.length, text.indexOf('Status'))
+            orderid = orderid.trim() 
+            email = text.substring(text.indexOf('E-Mail')+'E-Mail'.length, text.indexOf('Telephone'))
+            email = email.trim()
+            cy.log(orderid)
+            cy.log(email)
         })
 
     })
 
-    it('Brand selection and sorting', ()=>{
+    it.only('Brand selection and sorting', ()=>{
         cy.get('#brandcarousal > li:nth-child(1) > div.image > a > img').click()
         cy.get('#sort').select('p.price-ASC')
         cy.get(':nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').contains('BeneFit Girl Meets Pearl')
         cy.get(':nth-child(2) > .fixed_wrapper > .fixed > .prdocutname').contains('Benefit Bella Bamba')
         cy.get(':nth-child(3) > .fixed_wrapper > .fixed > .prdocutname').contains('Product with stock locations')
         
+        //getting the price and comparing.
+        cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(1) > div.thumbnail > div.pricetag.jumbotron > div > div.pricenew')
+        .invoke('text')
+        .then((text)=>{
+            str.push(text.substring(text.indexOf('$')+'$'.length, text.length))
+            parseFloat(str)
+        })
+
+        cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(2) > div.thumbnail > div.pricetag.jumbotron > div > div')
+        .invoke('text')
+        .then((text)=>{
+            str.push(text.substring(text.indexOf('$')+'$'.length, text.length))
+            parseFloat(str)
+            
+        })
+
+        cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(3) > div.thumbnail > div.pricetag.jumbotron > div > div')
+        .invoke('text')
+        .then((text)=>{
+            str.push(text.substring(text.indexOf('$')+'$'.length, text.length))
+            parseFloat(str)
+        })
+
+        cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(4) > div.thumbnail > div.pricetag.jumbotron > div > div')
+        .invoke('text')
+        .then((text)=>{
+            str.push(text.substring(text.indexOf('$')+'$'.length, text.length))
+            parseFloat(str)
+        })
+
+        cy.wrap(str).then((rez)=>{
+            cy.log(verifysort(rez))
+        })
+
+
     })
 
     it('Checking Out of stock items', () =>{
         cy.get('#categorymenu > nav > ul > li:nth-child(4) > div > ul:nth-child(1) > li:nth-child(5) > a').click({force:true})
         cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div > div.fixed_wrapper > div > a')
         .click()
+
         cy.get('#product > fieldset > div:nth-child(4) > ul > li > span').should('have.class', 'nostock')
         .contains('Out of Stock')
     })
@@ -69,21 +107,27 @@ function Adddata(name, lastname, email, teleph, fax, company, address, city, zip
     cy.get('#guestFrm_postcode').type(zip)
 }
 
-function catchstr(text)
+function verifysort(str)
 {
-    var str;
-    for(let i = 0; i< text.lenght; i++)
+    for(let i=0;i<str.length;i++)
     {
-        if(i == '#')
+        str[i] = parseFloat(str[i])
+    }
+    for(let i = 0; i< str.length;i++)
         {
-            while(i != ' ')
+            let ok = 0
+            if(str[i] < str[i+1] && i != str.length-1)
             {
-                str = text.slice(i,i+1)
-                i+=1;
+                ok=1
+            }
+            else
+            {
+                cy.log('Elements are not sorted in the right way.')
+                break;
+            }
+            if(i == str.length-1 && ok == 1)
+            {
+                cy.log('Elements are sorted in the right way.')
             }
         }
-    }
-    cy.log(str)
-    text = str
-    return text;
 }
