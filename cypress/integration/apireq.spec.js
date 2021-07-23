ae75c0c93503328edf7bcba9e198406648680463
IulianPinzaru <62741558+IulianPinzaru@users.noreply.github.com>
2021-07-23 10:00:05 +0300

    Api Req#2

--- cypress/integration/apireq.spec.js
+++ cypress/integration/apireq.spec.js
@@ -7,7 +7,15 @@ describe('API Request', ()=>{
 
     it('Api Request', ()=>{
         cy.request('https://petstore.swagger.io/v2/store/inventory').then((res) =>{
-
+            expect(res).to.have.property('status', 200)
+            console.log(res.body)
+            expect(res.body).to.not.be.null
+            /*expect(res.body).to.include('123')
+            expect(res.body).to.include('sold')
+            expect(res.body).to.include('string')
+            expect(res.body).to.include('pending')
+            expect(res.body).to.include('available')
+            */
             Object.entries(res.body).forEach(element =>{
                 cy.log(element[0] + ' ' + element[1])
             })
